586e86bdbffafb0417906b828c5a17eb30432d2a
Ivan Barynkin <ivan.barynkin08@gmail.com>
2025-09-02 20:03:26 +0300

    Init commit

---
+++ src/main/java/Service/StudentService.java
@@ -0,0 +1,10 @@
+package Service;
+
+import Model.Student;
+
+import java.util.HashMap;
+
+public class StudentService {
+
+    private HashMap<Long, Student> storage = new HashMap<Long,Student>();
+}
